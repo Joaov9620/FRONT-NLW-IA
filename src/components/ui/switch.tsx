e75f9fc7aa2878d056fb2f980884de728c61c4ed
joao <pjoaovictor22@hotmail.com>
2023-09-12 10:15:03 -0300

    feat: initial page home

---
+++ src/components/ui/switch.tsx
@@ -0,0 +1,42 @@
+import * as React from "react"
+import * as SwitchPrimitives from "@radix-ui/react-switch"
+
+import { Moon, SunMoon } from 'lucide-react'
+
+import { cn } from "@/lib/utils"
+
+// interface Props{
+//   isDarkMode: boolean;
+// }
+
+const isDarkMode = false;
+
+const Switch = React.forwardRef<
+  React.ElementRef<typeof SwitchPrimitives.Root>,
+  React.ComponentPropsWithoutRef<typeof SwitchPrimitives.Root>
+>(({ className, ...props } , ref) => (
+  <SwitchPrimitives.Root
+    className={cn(
+      "peer inline-flex h-[20px] w-[36px] shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=unchecked]:bg-input",
+      className
+    )}
+    {...props}
+    ref={ref}
+  >
+    <SwitchPrimitives.Thumb
+      className={cn(
+        "pointer-events-none block h-4 w-4 rounded-full bg-background shadow-lg ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0"
+      )}
+    />
+      {/* <div className="pointer-events-none block h-4 w-4 rounded-full ring-0 transition-transform data-[state=checked]:translate-x-4 data-[state=unchecked]:translate-x-0">
+      {isDarkMode ? (
+        <Moon className="w-4 h-4 mr-2"/>
+      ) : (
+        <SunMoon className="w-4 h-4 mr-2"/>
+      )}
+      </div> */}
+  </SwitchPrimitives.Root>
+))
+Switch.displayName = SwitchPrimitives.Root.displayName
+
+export { Switch }
